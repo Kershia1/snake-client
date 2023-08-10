8b6db6980d1c482f4d8a6f95a2775bd7197c7b9b
Kershia1 <insatiable.meerkat@gmail.com>
2023-08-10 02:42:17 +0000

    client.js

--- client.js
+++ client.js
@@ -11,10 +11,35 @@ const connect = function() {
 
 const conn = connect(); //  too call connect func, global scope
 
+//event name
 conn.on("connect", ()=> {
   conn.write("Name: RAY");
 });
 
+ //event move-up 
+//  conn.on("connect", () => {
+//   setInterval(() => {
+//     conn.write("Move: up");
+//   }, 50);
+//   conn.write("Move: up"); 
+//  });
 
+//  conn.on("connect", () => {
+//   setTimeout=(() => {
+//   conn.write("Move: left"); 
+//  }, 50);
+// });
+
+//  conn.on("connect", () => {
+//   setTimeout=(() => {
+//   conn.write("Move: down"); 
+//  }, 100);
+// });
+
+//  conn.on("connect", () => {
+//   setTimeout=(() => {
+//   conn.write("Move: right"); 
+//  },150);
+// });
 
 module.exports = {connect};  // to export connect func
