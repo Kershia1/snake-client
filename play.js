c864dbad6f875dfd93589e3b836edfd4c45e0e76
Kershia1 <insatiable.meerkat@gmail.com>
2023-08-10 04:46:08 +0000

    play.js

--- play.js
+++ play.js
@@ -1,20 +1,21 @@
 
-const net = require("net"); 
+const net = require("net");
 
-const {connect} = require("./client"); // require connect func from client.js
+const { connect } = require("./client"); // require connect func from client.js
 
 //allows for listening to key board input from input.js mod
-const {setupInput} = require("./input"); 
+const { setupInput } = require("./input");
 
 //call the linked functions
-const conn = connect(); 
-const stdin = setupInput(); 
+const conn = connect();
+setupInput(conn);
+// const stdin = setupInput();
 
-conn.on("data",(data)=>  {
-console.log("The server sent you a message: ", data);
+conn.on("data", (data) => {
+  console.log("The server sent you a message: ", data);
 }); //event handler for established connection
 
-conn.on("connect", ()=> {
+conn.on("connect", () => {
   console.log("Successfully connected to the game server");
 });
 conn.setEncoding("utf8");
@@ -25,3 +26,4 @@ connect(); // call connect func
 
 setupInput(); // call setupInput func
 
+module.exports = { connect };  // to export obj returned by connect func
