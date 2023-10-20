39b5fb2e408e7649f94b338e1fc67f976949ab2b
Kershia1 <insatiable.meerkat@gmail.com>
2023-10-20 12:10:12 -0700

    odified

--- constants.js
+++ constants.js
@@ -9,5 +9,3 @@ module.exports = {
   IP, 
   PORT, 
 };
-
-// module.exports = { constants };
--- play.js
+++ play.js
@@ -29,33 +29,3 @@ connect(); // call connect func
 setupInput(); // call setupInput func
 
 module.exports = { connect };  // to export obj returned by connect func
-
-/*
-De-Bug note: Used AI Assistance to understand event throw error. 
-Relavent Error:
-node:events:491
-      throw er; // Unhandled 'error' event
-      ^
-
-Error: connect ECONNREFUSED 127.0.0.1:50541
-    at TCPConnectWrap.afterConnect [as oncomplete] (node:net:1278:16)
-Emitted 'error' event on Socket instance at:
-    at emitErrorNT (node:internal/streams/destroy:157:8)
-    at emitErrorCloseNT (node:internal/streams/destroy:122:3)
-    at processTicksAndRejections (node:internal/process/task_queues:83:21) {
-  errno: -111,
-  code: 'ECONNREFUSED',
-  syscall: 'connect',
-  address: '127.0.0.1',
-  port: 50541
-}
-
-Assistance Used to then search on StackOverFlow: 
-Looks like something maybe using port 50541 still, incorrectly killed project?
-these are the insturctions to resolve, very vague 
-
-node:events:491
-https://stackoverflow.com/questions/75484596/npm-is-giving-error-for-instance-nodeevents491-throw-er-unhandled-error
-
-
-*/
